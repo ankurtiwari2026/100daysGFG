1a888e7ff70aaf124f984939c9bb71b160e4b51d
ankur <2k22.csiot.2212422@gmail.com>
2024-03-08 22:05:43 +0530

    Solutions

---
+++ 07-march-2024.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestSubstring(self, s , n):
+        
+        res,maxi = "",-1
+        d = {}
+        for i in range(n):
+            x = ""
+            for j in range(i,n):
+                x += s[j]
+                if x in s[j+1:]:
+                    if maxi<len(x):
+                        maxi = len(x)
+                        res = x
+                else:
+                    break
+        return res if res !="" else "-1"
