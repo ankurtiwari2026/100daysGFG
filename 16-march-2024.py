67ed52421bda05617ffd645c6eff91565a0c1bce
ankur <2k22.csiot.2212422@gmail.com>
2024-03-16 22:40:58 +0530

    Delete without head pointer

---
+++ 16-march-2024.py
@@ -0,0 +1,10 @@
+class Solution:
+    #Function to delete a node without any reference to head pointer.
+    def deleteNode(self,del_node):
+        if not del_node.next:
+            del_node=None
+        else:
+            del_node.data=del_node.next.data
+            del_node.next=del_node.next.next
+        
+        
