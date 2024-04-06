b32c4ce20028a7c8e7f785598aa4fe143d6460c3
ankur <2k22.csiot.2212422@gmail.com>
2024-04-06 09:02:44 +0530

    soluution

---
+++ 6-april-2024.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    #Function to count number of ways to reach the nth stair
+    #when order does not matter.
+    def countWays(self, n):
+        return n//2+1
+
+        mod = 1000000007
+        # code here
